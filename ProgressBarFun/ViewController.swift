771502d8b8a40c68974ced1494c05e54a4158e1d
Sotheavuth999 <sotheavuthn@yahoo.com>
2016-10-10 10:16:29 +0300

    first commit

--- ProgressBarFun/ViewController.swift
+++ ProgressBarFun/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBarView: ProgressBarView!
+    @IBOutlet weak var slider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        progressBarView.progress = CGFloat(slider.value)
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# Progress Bar in Swift Programming Language
